b5cf6d4dd9ef8ad18f84809a31646ae5b4367692
yusongh <45997005+yusongh@users.noreply.github.com>
2024-05-07 11:29:53 +0800

    feat: 新增 vite 拆包

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import visualizer from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react': ['react', 'react-dom'],
+          'antd': ['antd']
+        }
+      }
+    },
+  },
 })
